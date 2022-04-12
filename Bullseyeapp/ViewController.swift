0cf1fb75c592ada9be8200ff324c8781e5e280fe
Bruno Campos <bruno.mcamps@outlook.com>
2022-04-12 17:05:59 -0300

    Implemented difference

--- Bullseyeapp/ViewController.swift
+++ Bullseyeapp/ViewController.swift
@@ -26,8 +26,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
+        
+        //var difference: Int
+        
+//        if currentValue > targetValue {
+//            difference = currentValue - targetValue
+//        } else if targetValue > currentValue {
+//            difference = targetValue - currentValue
+//        } else {
+//            difference = 0
+//        }
+        
+        var difference = currentValue - targetValue
+        if difference < 0 {
+            difference = difference * -1
+        }
+        
+        
         let message = "The value of the slider is: \(currentValue)" +
-        "\nThe target value is: \(String(describing: targetValue))"
+        "\nThe target value is: \(String(describing: targetValue))" +
+        "\nThe difference is: \(difference)"
 
         let alert = UIAlertController(
           title: "Hello, World",
